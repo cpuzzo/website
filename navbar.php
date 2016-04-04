91df4c787e37e390bce61d0fcda752068094d005
Chris Puzzo <cpuzzo@24-m.com>
2016-04-04 18:12:30 -0400

    Added files via upload

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<?php>        
+
+echo '<div id="navbar">
+        <ul id="navlist">
+            <li><span class="oneLine"><a href="../index.html">Home</a></span></li>
+            <li><span class="oneLine"><a href="../Resume.pdf">Resume</a></span></li>
+            <li><span class="twoLine"><a href="../WritingSamples.html">Writing<br/> Samples</a></span></li>
+            <li><span class="twoLine"><a href="../CodeExamples.html">Code<br/> Examples</a></span></li>
+            <li><span class="oneLine"><a href="../PastProjects.html">Projects</a></span></li>
+            <li><span class="oneLine"><a href="../Contact.html">Contact</a></span></li>
+        </ul>
+    </div>';
+    
+</php>
