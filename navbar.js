75d82c22918dd75591b3140c4c1039fd0de9468a
Chris Puzzo <cpuzzo@24-m.com>
2016-04-06 13:53:57 -0400

    Continued work on website
    Created all pages, as well as robots.txt.
    
    Contact page "finished"
    
    Minor changes to main.css

---
+++ navbar.js
@@ -0,0 +1,8 @@
+document.getElementById('navbar').innerHTML = '<ul id="navlist">' +
+    '<li><span class="oneLine"><a href="../index.html">Home</a></span></li>' +
+    '<li><span class="oneLine"><a href="../puzzo_resume.html">Resume</a></span></li>' +
+    '<li><span class="twoLine"><a href="../writing_samples.html">Writing<br/>Samples</a></span></li>' +
+    '<li><span class="twoLine"><a href="../code_samples.html">Code<br/> Examples</a></span></li>' +
+    '<li><span class="oneLine"><a href="../projects.html">Projects</a></span></li>' +
+    '<li><span class="oneLine"><a href="../contact.html">Contact</a></span></li>' +
+    '</ul>';
