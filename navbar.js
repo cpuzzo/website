7b3c561ea0d2140980a91bde9042caed61a97a45
Chris Puzzo <cpuzzo@24-m.com>
2016-04-06 14:34:55 -0400

    More work, mostly on projects/code sample
    Still need real content though...

--- navbar.js
+++ navbar.js
@@ -1,8 +1,9 @@
-document.getElementById('navbar').innerHTML = '<ul id="navlist">' +
+document.getElementById('navbar').innerHTML = '<div style="height:4em;"><hr/>' +
+    '<ul id="navlist">' +
     '<li><span class="oneLine"><a href="../index.html">Home</a></span></li>' +
     '<li><span class="oneLine"><a href="../puzzo_resume.html">Resume</a></span></li>' +
     '<li><span class="twoLine"><a href="../writing_samples.html">Writing<br/>Samples</a></span></li>' +
     '<li><span class="twoLine"><a href="../code_samples.html">Code<br/> Examples</a></span></li>' +
     '<li><span class="oneLine"><a href="../projects.html">Projects</a></span></li>' +
     '<li><span class="oneLine"><a href="../contact.html">Contact</a></span></li>' +
-    '</ul>';
+    '</ul></div><hr/>';
